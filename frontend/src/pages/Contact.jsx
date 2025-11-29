f454a64f6afe03e4959b150f3fc9f4420e604570
GreeshKolla123 <2100030262@kluniversity.in>
2025-11-29 15:42:32 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Get in Touch</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
