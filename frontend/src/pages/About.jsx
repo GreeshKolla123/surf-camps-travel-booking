056e1791ce60ac6771a243a68378776b667547e1
GreeshKolla123 <2100030262@kluniversity.in>
2025-11-29 15:42:27 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
